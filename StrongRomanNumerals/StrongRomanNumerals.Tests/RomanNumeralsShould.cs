6e666fe5afcd040a48964d92a6f1c07d776f7064
Peter Street <lazerfx@gmail.com>
2022-02-15 09:20:25 +0000

    Theory

--- StrongRomanNumerals/StrongRomanNumerals.Tests/RomanNumeralsShould.cs
+++ StrongRomanNumerals/StrongRomanNumerals.Tests/RomanNumeralsShould.cs
@@ -4,11 +4,13 @@ namespace StrongRomanNumerals.Tests;
 
 public class RomanNumeralsShould
 {
-    [Fact(DisplayName = "convert 1 to I")]
-    public void Test1()
+    [Theory(DisplayName = "convert 1 to I")]
+    [InlineData(1, "I")]
+    [InlineData(2, "II")]
+    public void Convert(int i, string expectation)
     {
         var romanNumerals = new RomanNumerals();
-        var result = romanNumerals.Convert(1);
-        Assert.Equal("I", result);
+        var result = romanNumerals.Convert(i);
+        Assert.Equal(expectation, result);
     }
 }
