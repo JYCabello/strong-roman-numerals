86334fd4cc4241a6f5c3381e14142000b662e522
Peter Street <lazerfx@gmail.com>
2022-02-15 09:28:44 +0000

    Green

--- StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
+++ StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
@@ -2,6 +2,10 @@
 
 public class RomanNumerals
 {
-    public string Convert(int i) =>
-        string.Join("", Enumerable.Range(0, i).Select(_ => "I"));
+    public string Convert(int i)
+    {
+        if (i == 4) { return "IV"; }
+
+        return string.Join("", Enumerable.Range(0, i).Select(_ => "I"));
+    }
 }
