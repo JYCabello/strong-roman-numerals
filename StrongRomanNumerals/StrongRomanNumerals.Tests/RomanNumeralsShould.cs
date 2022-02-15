07679bbc1836e5295bdfda5183b9ba96660baace
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-15 10:26:49 +0100

    red

--- StrongRomanNumerals/StrongRomanNumerals.Tests/RomanNumeralsShould.cs
+++ StrongRomanNumerals/StrongRomanNumerals.Tests/RomanNumeralsShould.cs
@@ -7,6 +7,8 @@ public class RomanNumeralsShould
     [Theory(DisplayName = "convert 1 to I")]
     [InlineData(1, "I")]
     [InlineData(2, "II")]
+    [InlineData(3, "III")]
+    [InlineData(4, "IV")]
     public void Convert(int i, string expectation)
     {
         var romanNumerals = new RomanNumerals();
