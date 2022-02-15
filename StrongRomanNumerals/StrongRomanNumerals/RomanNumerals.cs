a56ef201179a292a7d96527714829f327bc677e8
Peter Street <lazerfx@gmail.com>
2022-02-15 09:44:25 +0000

    Green

--- StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
+++ StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
@@ -5,8 +5,6 @@ public class RomanNumerals
     private static readonly Dictionary<int, string> Lookup = new()
     {
         { 1, "I" },
-        { 2, "II" },
-        { 3, "III" },
         { 4, "IV" },
         { 5, "V" }
     };
