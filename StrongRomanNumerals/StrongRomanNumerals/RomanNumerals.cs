dae8ef3d46d0f1bbf7d6ffe7572ea956dc8533c4
Peter Street <lazerfx@gmail.com>
2022-02-16 09:14:22 +0000

    green

--- StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
+++ StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
@@ -2,6 +2,17 @@
 
 public class RomanNumerals
 {
-    public string Convert(int input) =>
-        input == 1 ? "I" : "V";
+    public string Convert(int input)
+    {
+        switch (input) {
+            case 1:
+                return "I";
+            case 4:
+                return "IV";
+            case 5:
+                return "V";
+            default:
+                return "";
+        }
+    }
 }
