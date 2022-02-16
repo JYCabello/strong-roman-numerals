c6814c8a9c20e05cc615c4908be619b1c390a0cf
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-16 09:58:36 +0100

    clean slate

--- StrongRomanNumerals/StrongRomanNumerals.Tests/RomanNumeralsShould.cs
+++ StrongRomanNumerals/StrongRomanNumerals.Tests/RomanNumeralsShould.cs
@@ -4,17 +4,9 @@ namespace StrongRomanNumerals.Tests;
 
 public class RomanNumeralsShould
 {
-    [Theory(DisplayName = "convert 1 to I")]
-    [InlineData(1, "I")]
-    [InlineData(2, "II")]
-    [InlineData(3, "III")]
-    [InlineData(4, "IV")]
-    [InlineData(5, "V")]
-    [InlineData(6, "VI")]
-    public void Convert(int i, string expectation)
+    [Fact(DisplayName = "convert 1 to I")]
+    public void Convert()
     {
-        var romanNumerals = new RomanNumerals();
-        var result = romanNumerals.Convert(i);
-        Assert.Equal(expectation, result);
+
     }
 }
--- StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
+++ StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
@@ -2,28 +2,5 @@
 
 public class RomanNumerals
 {
-    private static readonly Dictionary<int, string> Lookup = new()
-    {
-        { 1, "I" },
-        { 4, "IV" },
-        { 5, "V" }
-    };
 
-    public string Convert(int i)
-    {
-        return Go(i, "");
-        
-        string Go(int index, string value)
-        {
-            if (index == 0) {  return value; }
-
-            var highestMatch = Lookup
-                .Keys
-                .OrderByDescending(k => k)
-                .First(k => k <= index);
-            var symbol = Lookup[highestMatch];
-
-            return Go(index - highestMatch, value + symbol);
-        }
-    }
 }
