012c2e9712b62a1c668f610455ab6e5d8f370286
Peter Street <lazerfx@gmail.com>
2022-02-16 09:28:31 +0000

    Refactor

--- StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
+++ StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
@@ -2,25 +2,24 @@
 
 public class RomanNumerals
 {
-    public static readonly Dictionary<int, string> numeralMap =
-        new()
+    public static readonly IOrderedEnumerable<KeyValuePair<int, string>> numeralMap =
+        new Dictionary<int,string>()
         {
             { 1, "I" },
             { 4, "IV" },
             { 5, "V" },
             { 10, "X" }
-        };
+        }.OrderByDescending(kv => kv.Key);
 
     public string Convert(int input)
     {
         var (number, symbol) =
             numeralMap
-                .OrderByDescending(kv => kv.Key)
                 .First(kv => input >= kv.Key);
 
         if (input - number >= number)
             return symbol + Convert(input - number);
 
-        return numeralMap[input];
+        return numeralMap.Single(kv => kv.Key == input).Value;    
     }
 }
