b78d3c237a38c9e4b23da063f158842e86b023c4
Peter Street <lazerfx@gmail.com>
2022-02-16 09:16:01 +0000

    Refactor

--- StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
+++ StrongRomanNumerals/StrongRomanNumerals/RomanNumerals.cs
@@ -2,17 +2,16 @@
 
 public class RomanNumerals
 {
+    public static Dictionary<int, string> numeralMap =>
+        new()
+        {
+            { 1, "I" },
+            { 4, "IV" },
+            { 5, "V" }
+        };
+
     public string Convert(int input)
     {
-        switch (input) {
-            case 1:
-                return "I";
-            case 4:
-                return "IV";
-            case 5:
-                return "V";
-            default:
-                return "";
-        }
+        return numeralMap[input];
     }
 }
